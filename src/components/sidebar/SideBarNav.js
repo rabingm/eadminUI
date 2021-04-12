2cbbd5dea71461bda79c664ff0d93498f510a6ba
Rabin Ghimire <rabinghimire2012@gmail.com>
2021-04-13 09:30:57 +1000

    sidebar link updated

--- src/components/sidebar/SideBarNav.js
+++ src/components/sidebar/SideBarNav.js
@@ -7,12 +7,12 @@ export const SideBarNav = () => {
         <div className="sidebar-nav">
             <ul>
                 <li>
-                   <Link to="/dashboard">Dashboard</Link> </li>
-                <li><Link to="/category">Category</Link></li>
-                <li><Link to="/products">Products</Link></li>
-                <li><Link to="/orders">Orders</Link></li>
-                <li><Link to="/users">Users</Link></li>
-                <li><Link to="/accounts">Account</Link></li>
+                   <Link to="/dashboard"><i class="fas fa-tachometer-alt"></i>  Dashboard</Link> </li>
+                <li><Link to="/category"><i class="fas fa-sitemap"></i>Category</Link></li>
+                <li><Link to="/products"><i class="fab fa-expeditedssl"></i>Products</Link></li>
+                <li><Link to="/orders"><i class="fas fa-cart-arrow-down"></i>Orders</Link></li>
+                <li><Link to="/users"><i class="fas fa-user"></i>Users</Link></li>
+                <li><Link to="/accounts"><i class="fas fa-user-circle"></i>Account</Link></li>
 
             </ul>
         </div>
