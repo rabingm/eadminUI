f8758495d5ad22c8a06ac20925e8abf5a56460f2
Rabin Ghimire <rabinghimire2012@gmail.com>
2021-04-13 08:55:27 +1000

    sidebar link updated

--- src/components/sidebar/SideBarNav.js
+++ src/components/sidebar/SideBarNav.js
@@ -8,11 +8,11 @@ export const SideBarNav = () => {
             <ul>
                 <li>
                    <Link to="/dashboard">Dashboard</Link> </li>
-                <li><Link to="/dashboard">Category</Link></li>
-                <li><Link to="/dashboard">Products</Link></li>
-                <li><Link to="/dashboard">Orders</Link></li>
-                <li><Link to="/dashboard">Users</Link></li>
-                <li><Link to="/dashboard">Account</Link></li>
+                <li><Link to="/category">Category</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/orders">Orders</Link></li>
+                <li><Link to="/users">Users</Link></li>
+                <li><Link to="/accounts">Account</Link></li>
 
             </ul>
         </div>
